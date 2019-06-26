34779080f187c0d08b08e6bf86614fd39b7113ef
keitmark <keitmark@mail.ru>
2019-06-26 09:23:39 +0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+   
+
+    // $('.tel-inp').inputmask({
+    //     "mask": "+7 (999) 999-99-99"
+    //     , "placeholder": "_"
+    //     , showMaskOnHover: false
+    //     , showMaskOnFocus: true
+    // });
+
+    // function scrollMenus() {
+    //     if($(window).scrollTop() > 300){
+    //         $('.header__bott').addClass('fixed-style');
+    //       }
+    //       else {
+    //         $('.header__bott').removeClass('fixed-style');
+    //     }
+    // }
+   
+    // scrollMenus();
+
+    // $(window).scroll(function(){
+    //     scrollMenus();
+    // });
+    
+});
+
